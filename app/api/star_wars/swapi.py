fda75ea51574c167a2d568e60cfb00c451f3e71f
Francisco Arocas <54645175+franciscoarocas@users.noreply.github.com>
2025-06-21 01:06:41 +0100

    Added caché and error when api unavailable

--- app/api/star_wars/swapi.py
+++ app/api/star_wars/swapi.py
@@ -1,8 +1,17 @@
 
 from app.api.star_wars.base import StarWarsAPIBase
 
+from fastapi import HTTPException
+
+from cachetools import TTLCache, cached
+
 import httpx
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+cache = TTLCache(maxsize=1024, ttl=60)
 
 class Swapi(StarWarsAPIBase):
     """
@@ -15,7 +24,7 @@ class Swapi(StarWarsAPIBase):
     def __init__(self) -> None:
         super().__init__(self.__API_URL)
 
-    def parse_people_reponse_data(self, data):
+    def parse_people_response_data(self, data):
         return data
 
     async def get_people(self, *args, **kwargs):
@@ -23,18 +32,25 @@ class Swapi(StarWarsAPIBase):
         Fetches people from the SWAPI.
         """
 
+        if 'people' in cache:
+            return cache['people']
+
         try:
           async with httpx.AsyncClient() as client:
               response = await client.get(f"{self.api_url}/people")
               response.raise_for_status()
-              parsed_people = self.parse_people_reponse_data(response.json())
+              parsed_people = self.parse_people_response_data(response.json())
+              cache["people"] = parsed_people
               return parsed_people
         except httpx.HTTPStatusError as e:
-            # TODD: Hacer un manejo de errores más específico
-            pass
+            logger.error(f"Error fetching people API: {e}")
+            raise HTTPException(
+                status_code=503,
+                detail="Resource temporarily unavailable. Please try again later."
+            )
 
 
-    def parse_planets_reponse_data(self, data):
+    def parse_planets_response_data(self, data):
         return data
 
 
@@ -43,12 +59,19 @@ class Swapi(StarWarsAPIBase):
         Fetches planets from the SWAPI.
         """
 
+        if 'planets' in cache:
+            return cache['planets']
+
         try:
           async with httpx.AsyncClient() as client:
               response = await client.get(f"{self.api_url}/planets")
               response.raise_for_status()
-              parsed_planets = self.parse_planets_reponse_data(response.json())
+              parsed_planets = self.parse_planets_response_data(response.json())
+              cache["planets"] = parsed_planets
               return parsed_planets
         except httpx.HTTPStatusError as e:
-            # TODD: Hacer un manejo de errores más específico
-            pass
+            logger.error(f"Error fetching planets API: {e}")
+            raise HTTPException(
+                status_code=503,
+                detail="Resource temporarily unavailable. Please try again later."
+            )
