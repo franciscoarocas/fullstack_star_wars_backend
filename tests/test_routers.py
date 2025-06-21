6d8cc801b1c95817525e68eab520bedcc25319e0
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2025-06-21 23:08:09 +0200

    deleted comments

--- tests/test_routers.py
+++ tests/test_routers.py
@@ -11,12 +11,9 @@ from tests.factory.api import build_fake_response
 @pytest.mark.asyncio
 @patch("app.api.star_wars.swapi.httpx.Client.get", new_callable=MagicMock)
 async def test_people_endpoint(mock_get):
-    # fuerza ejecución real del servicio (opcional si hay caché)
 
-    # mock de la llamada a la API externa
     mock_get.side_effect = build_fake_response(build_fake_swapi_people_data())
 
-    # transport ASGI que ejecuta la app en el MISMO hilo
     transport = httpx.ASGITransport(app=app)
     async with httpx.AsyncClient(transport=transport, base_url="http://test") as ac:
         res = await ac.get("/people/")
